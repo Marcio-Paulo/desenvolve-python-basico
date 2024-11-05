0c9b03d73559a66a3c81fa48a424b01eb792afc8
Marcio Paulo <164260459+Marcio-Paulo@users.noreply.github.com>
2024-11-05 20:49:17 -0300

    Add files via upload

---
+++ Modulo 2 - Tipos de Dados/aula2_questao3.py
@@ -0,0 +1,9 @@
+v1 = 10
+v2 = 20
+
+v3 = v1
+v1 = v2
+v2 = v3
+
+print("Valor de v1 após a troca:", v1)
+print("Valor de v2 após a troca:", v2)
---
+++ Modulo 2 - Tipos de Dados/aula2_questao4.py
@@ -0,0 +1,8 @@
+juros = 1.01
+saldo = 500
+saldo = saldo * juros
+print(saldo)
+saldo = saldo * juros
+print(saldo)
+saldo = saldo * juros
+print(saldo)
